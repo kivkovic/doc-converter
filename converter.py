be0d16ac26187cd108a4924635809a7c99db6b1d
Kresimir Ivkovic <kresimir.ivkovic@eci.nl>
2017-12-17 11:58:47 +0100

    Use post-processor for doc-to-html

--- converter.py
+++ converter.py
@@ -1,6 +1,11 @@
 import os
 import re
 
+import sys
+sys.path.append('./postprocessors')
+
+from doc2html import DocToHTMLPostProcessor
+
 
 def convert(file_path, target_format, executable='libreoffice'):
 
@@ -12,5 +17,8 @@ def convert(file_path, target_format, executable='libreoffice'):
     response = os.system(' '.join([
         executable, '--headless', '--invisible', '--convert-to', target_format, '--outdir', outdir, file_path
     ]))
+
+    if target_format == 'html' and re.match('.*\.(docx?|odt)$', file_path, re.IGNORECASE):
+        DocToHTMLPostProcessor(re.sub('^(.+)\.(docx?|odt)$', '\\1.html', file_path, re.IGNORECASE))
 
     return response == 0
