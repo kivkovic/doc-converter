570fb08c7bebe651dc23453aeb16be752a2736ef
Kresimir Ivkovic <kresimir.ivkovic@eci.nl>
2017-12-17 16:16:02 +0100

    Added simple CLI

--- converter.py
+++ converter.py
@@ -21,8 +21,63 @@ def convert(file_path, target_format, executable='libreoffice', local_fonts = Fa
     response = os.system(command)
 
     if target_format == 'html' and re.match('.*\.(docx?|odt)$', file_path, re.IGNORECASE):
-        DocToHTMLPostProcessor(re.sub('^(.+)\.(docx?|odt)$', '\\1.html', file_path, re.IGNORECASE), local_fonts = local_fonts, font_alternatives = font_alternatives)
+        DocToHTMLPostProcessor(re.sub('^(.+)\.(docx?|odt)$', '\\1.html', file_path, re.IGNORECASE), local_fonts = local_fonts, font_alternatives = font_alternatives, inline_images = inline_images)
 
     shutil.rmtree(temp_profile_dir)
 
     return response == 0
+
+
+if __name__ == '__main__':
+
+    import getopt
+
+    def usage():
+        print('python converter.py -s [FILE] -f [FORMAT] [-e [LIBREOFFICE_PATH]] [-l [FONTS_DIR]] [-a [FONT_ALTERNATIVES_FILE]] [-i]')
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 's:f:e:l:a:ih')
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    file_path = None
+    target_format = None
+    executable='libreoffice'
+    local_fonts = False
+    font_alternatives = False
+    inline_images = False
+
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit(2)
+        elif opt == '-s':
+            file_path = arg
+        elif opt == '-f':
+            target_format = arg
+        elif opt == '-e':
+            executable = arg
+        elif opt == '-l':
+            local_fonts = arg
+            if not local_fonts[-1] in ['/','\\']:
+                local_fonts += '/'
+        elif opt == '-a':
+            font_alternatives = arg
+        elif opt == '-i':
+            inline_images = True
+
+        else:
+            usage()
+            sys.exit(2)
+
+    if not file_path:
+        usage()
+        raise Exception('Source file not specified')
+
+    if not target_format:
+        usage()
+        raise Exception('Target format not specified')
+
+    result = convert(file_path, target_format, executable, local_fonts, font_alternatives, inline_images)
+    print(result and 'OK' or 'UNSPECIFIED ERROR')
