e5417fa8aba5da40624b91405dd1f4bfd14c49b8
Kresimir Ivkovic <kivkovic@gmail.com>
2019-06-17 12:43:52 +0200

    Replaced os cli call with popen + timeout, added libreoffice binary resolution from unoconv, added logging

--- converter.py
+++ converter.py
@@ -1,64 +1,115 @@
-import os, shutil, re, sys, tempfile
+import os, subprocess, shutil, re, sys, glob, tempfile, time, logging
 
-sys.path.append('./postprocessors')
+#sys.path.append('./postprocessors')
+#from doc2html import DocToHTMLPostProcessor
 
-from doc2html import DocToHTMLPostProcessor
-
-
-def convert(file_path, target_format, output_path = None, executable='libreoffice', local_fonts = False, font_alternatives = False, inline_images = True):
+def convert(file_path, target_format, output_path = None, executable = None, local_fonts = False, font_alternatives = False, inline_images = True, proc_timeout = 10000):
 
     if not os.path.isfile(file_path):
         raise Exception('File not found')
 
-    output_path = output_path or re.sub('^(.+)[/\\\\][^/\\\\]+$', '\\1', file_path)
+    if executable:
+        executable_root = re.sub('[\\/][^\\/]*$', '', executable)
+        executable_file = re.sub('^.*[\\/]*', '', executable)
 
-    temp_profile_dir = tempfile.mkdtemp(prefix='doc-converter_').replace('\\', '/')
-    env_override_user = '-env:UserInstallation=file:///' + temp_profile_dir + ''
+    else:
+        paths = glob.glob(os.environ['PROGRAMFILES'] + '\\LibreOffice*') + \
+            glob.glob(os.environ['PROGRAMFILES(X86)'] + '\\LibreOffice*') + \
+            glob.glob('/opt/libreoffice*') + \
+            glob.glob('/usr/local/libreoffice*')
 
-    command = ' '.join([
-        executable,
+        for path in paths:
+            for subdir in ['program', '']:
+                for binary in ['soffice.exe', 'soffice.bin', 'soffice']:
+                    if os.path.isfile(os.path.realpath(path + '/' + subdir + '/' + binary)):
+                        executable_root = os.path.realpath(path + '/' + subdir)
+                        executable_file = binary
+                        break
+
+    output_path = output_path or re.sub('^(.+)[/\\][^/\\]+$', '\\1', file_path)
+    temp_profile_dir = tempfile.mkdtemp(prefix = 'doc-converter_').replace('\\', '/')
+    env_override_user = '-env:UserInstallation=file:///' + temp_profile_dir
+
+    arguments = [
+        executable_file,
         env_override_user,
         '--headless',
         '--invisible',
+        '--nocrashreport',
+        '--nodefault',
+        '--nofirststartwizard',
+        '--nologo',
+        '--norestore',
         '--convert-to', target_format,
         '--outdir', output_path,
         file_path
-    ])
+    ]
 
-    response = os.system(command)
+    log.info('Calling subprocess.Popen with arguments: ' + ' '.join(arguments))
 
-    if target_format == 'html' and re.match('.*\.(docx?|odt)$', file_path, re.IGNORECASE):
-        DocToHTMLPostProcessor(
-            re.sub('^(.+)\.(docx?|odt)$', '\\1.html', file_path, re.IGNORECASE),
-            output_path = output_path,
-            local_fonts = local_fonts,
-            font_alternatives = font_alternatives,
-            inline_images = inline_images
-        )
+    libreprocess = subprocess.Popen(arguments, executable = executable_root + '/' + executable_file, env = os.environ)
+    timeout = 0
 
+    while True:
+        returncode = libreprocess.poll()
+
+        if returncode != None:
+            if returncode == 0:
+                log.info('Return code: ' + str(returncode))
+                libreprocess.wait()
+                shutil.rmtree(temp_profile_dir)
+                return True
+            else:
+                log.error('Return code: ' + str(returncode))
+                break
+
+        timeout += 1000
+        if timeout <= proc_timeout:
+            time.sleep(1)
+        else:
+            break
+
+    log.error('Process timed out after ' + proc_timeout + 'ms')
+    if libreprocess:
+        try:
+            libreprocess.kill()
+            log.info('Sent kill to process')
+        except AttributeError:
+            os.kill(libreprocess.pid, 9)
+            log.info('Sent kill 9 to process')
+
+    time.sleep(3)
     shutil.rmtree(temp_profile_dir)
 
-    return response == 0
+    return False
 
+
+log = type('', (), {})()
+log.log = log.debug = log.info = log.warning = log.error = log.exception = log.critical = lambda x: x
 
 if __name__ == '__main__':
 
     import getopt
 
     def usage():
-        print('python converter.py -i [INPUT_FILE] -o [OUTPUT_FILE] -f [FORMAT] [--executable=[LIBREOFFICE_PATH]] [--local-fonts=[LOCAL_FONTS]] [-font-alternatives=[FONT_ALTERNATIVES]] [--inline-images]')
+        print('python converter.py -i [INPUT_FILE] -o [OUTPUT_FILE] -f [FORMAT] [--executable=[LIBREOFFICE_PATH]]')
+        #print('python converter.py -i [INPUT_FILE] -o [OUTPUT_FILE] -f [FORMAT] [--executable=[LIBREOFFICE_PATH]] [--local-fonts=[LOCAL_FONTS]] [-font-alternatives=[FONT_ALTERNATIVES]] [--inline-images]')
+
+    log = logging.getLogger(__name__)
+    logging.basicConfig(filename = 'doc-converter.log', level = logging.INFO, format = '%(asctime)-15s %(message)s')
+    log.info('Started doc-converter with arguments: ' + ' '.join(sys.argv[1:]))
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'i:o:f:', ['executable=', 'local-fonts=', 'font-alternatives=', 'inline-images'])
     except getopt.GetoptError as e:
-        print(e)
+        log.error(e)
         usage()
         sys.exit(2)
 
     file_path = None
     target_format = None
     output_path = None
-    executable='libreoffice'
+    executable = None
     local_fonts = False
     font_alternatives = False
     inline_images = False
@@ -96,5 +147,4 @@ if __name__ == '__main__':
         usage()
         raise Exception('Target format not specified')
 
-    result = convert(file_path, target_format, output_path, executable, local_fonts, font_alternatives, inline_images)
-    print(result and 'OK' or 'UNSPECIFIED ERROR')
+    convert(file_path, target_format, output_path, executable, local_fonts, font_alternatives, inline_images)
